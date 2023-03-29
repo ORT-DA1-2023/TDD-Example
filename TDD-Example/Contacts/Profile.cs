d51f60a0b5fe687cb1fed71861b6f26b4599cc98
Facundo Arancet <facuarancet97@gmail.com>
2023-03-29 12:44:40 -0300

    [Green] Create profile success test

---
+++ TDD-Example/Contacts/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Contacts
+{
+    public class Profile
+    {
+        public Profile()
+        {
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string StreetAddress { get; set; }
+        public string PicturePath { get; set; }
+    }
+}
--- TDD-Example/ContantTests/ProfileTests.cs
+++ TDD-Example/ContantTests/ProfileTests.cs
@@ -1,14 +1,43 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Contacts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace ContantTests
+namespace ContactTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ProfileTests
     {
+        private const string John = "John";
+        private const string Doe = "Doe";
+        private readonly DateTime Oct7th1997 = new DateTime(1997, 10, 7);
+        private const string PhoneNumberSample = "+59899999999";
+        private const string SomeStreetAddress = "SomeStreet 1111";
+        private const string PicturePathSample = @"Resources\test-image.jpg";
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void CreateProfileSuccessTest()
         {
+            var profile = new Profile()
+            {
+                FirstName = John,
+                LastName = Doe,
+                BirthDate = Oct7th1997,
+                PhoneNumber = PhoneNumberSample,
+                StreetAddress = SomeStreetAddress,
+                PicturePath = PicturePathSample,
+            };
+
+            Assert.IsNotNull(profile);
+            Assert.AreEqual(profile.FirstName, John);
+            Assert.AreEqual(profile.LastName, Doe);
+            Assert.AreEqual(profile.BirthDate, Oct7th1997);
+            Assert.AreEqual(profile.PhoneNumber, PhoneNumberSample);
+            Assert.AreEqual(profile.StreetAddress, SomeStreetAddress);
+            Assert.AreEqual(profile.PicturePath, PicturePathSample);
+
         }
+
+
     }
 }
